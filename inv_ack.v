fd6fcacba2c1ba342ee78837ccbbc42c927e9806
Anshuman Mohan <anshumanmohan@live.com>
2019-03-15 16:57:26 +0800

    Code cleanup that should have no computational effect.

--- inv_ack.v
+++ inv_ack.v
@@ -10,7 +10,7 @@ Require Import applications.
 =============================================================================
 **************** SECTION 6: INVERSE ACKERMANN FUNCTION **********************
 =============================================================================
-*)
+ *)
 
 (* This section contains the most important application of countdown,
    the Inverse Ackermann function.
@@ -22,55 +22,50 @@ Require Import applications.
 (* *********** 6.1. INVERSE ACKERMANN HIERARCHY ******************* *)
 
 Fixpoint inv_ack_hier m x :=
-match m with
-| 0    => x - 1
-| S m' => countdown_to 1 (inv_ack_hier m') (inv_ack_hier m' x)
-end.
-
+  match m with
+  | 0 => x - 1
+  | S m' => countdown_to 1 (inv_ack_hier m') (inv_ack_hier m' x)
+  end.
 
 Theorem inv_ack_hier_correct :
-forall m, countdownable_to 1 (inv_ack_hier m)
-/\ upp_inv_rel (inv_ack_hier m) (ackermann m).
+  forall m, countdownable_to 1 (inv_ack_hier m) /\
+            upp_inv_rel (inv_ack_hier m) (ackermann m).
 Proof.
-induction m.
-{ split.
-  - split; intro x; simpl; omega.
-  - intros n x. simpl. omega. }
-destruct IHm as [IHm0 IHm1].
-split.
-- simpl. split; intro n;
-  rewrite countdown_to_antirecursion by apply IHm0;
-  apply (countdown_countdownable 1 _ 1) in IHm0;
-  destruct IHm0 as [IHm00 IHm01].
-  + specialize (IHm00 n). omega.
-  + trivial.
-  + specialize (IHm01 n). omega.
-  + trivial.
-- intros n x.
-  simpl. rewrite repeater_from_repeat.
-  rewrite <- repeat_S_comm.
-  apply (upp_inv_repeat (S n) _ _) in IHm1.
-  rewrite <- (IHm1 1 x).
-  rewrite repeat_S_comm.
-  apply countdown_to_repeat.
-  apply IHm0.
+  induction m.
+  { split.
+    - split; intro x; simpl; omega.
+    - intros n x. simpl. omega. }
+  destruct IHm as [IHm0 IHm1]. split.
+  - simpl. split; intro n;
+             rewrite countdown_to_antirecursion by apply IHm0;
+             apply (countdown_countdownable 1 _ 1) in IHm0;
+             destruct IHm0 as [IHm00 IHm01]; trivial.
+    + specialize (IHm00 n). omega.
+    + specialize (IHm01 n). omega.
+  - intros n x. simpl. rewrite repeater_from_repeat.
+    rewrite <- repeat_S_comm.
+    apply (upp_inv_repeat (S n) _ _) in IHm1.
+    rewrite <- (IHm1 1 x), repeat_S_comm.
+    apply countdown_to_repeat. apply IHm0.
 Qed.
 
 
 (* *********** 6.2. INVERSE ACKERMANN FUNCTION ******************** *)
 
 Fixpoint inv_ack_worker (f : nat -> nat) (n thr bud : nat) : nat :=
-match bud with
-| 0 => 0
-| S bud' => match (n - thr) with
-            | 0 => 0
-            | S _ => match (countdown_to 1 f) with
-                     g => S (inv_ack_worker (compose g f) (g n) (S thr) bud') end
-            end
-end.
+  match bud with
+  | 0 => 0
+  | S bud' =>
+    match (n - thr) with
+    | 0 => 0
+    | S _ =>
+      match (countdown_to 1 f) with
+        g => S (inv_ack_worker (compose g f) (g n) (S thr) bud') end
+    end
+  end.
 
-Definition inv_ack n := match (inv_ack_hier 0) with
-                        f => inv_ack_worker f (f n) 0 n end.
+Definition inv_ack n :=
+  match (inv_ack_hier 0) with f => inv_ack_worker f (f n) 0 n end.
 
 
 (* Compute inv_ack 7. *)
