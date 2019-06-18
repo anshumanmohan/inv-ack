3054c3fbdc0d505165e5072826640863de684293
Anshuman Mohan <anshumanmohan@live.com>
2019-06-18 17:36:23 +0800

    Cleaning codebase

--- prelims.v
+++ prelims.v
@@ -6,31 +6,33 @@ Require Import Setoid.
 =============================================================================
 ****************** SECTION 1: PRELIMINARIES *********************************
 =============================================================================
+*)
+
+(* 
+ * This file contains recurrently useful results and definitions
+ * that are used throughout the subsequent files. 
  *)
 
-(* This file contains recurrently useful results and definitions
-   that are used throughout the subsequent files *)
 
-
-(* ****** USEFUL LEMMAS ABOUT NAT ********************** *)
+(* ****** LEMMAS ABOUT NAT  ****** *)
 
 Lemma le_S_n_m : forall n m : nat, n <= m <-> S n <= S m.
-  intros. omega. Qed.
+Proof. intros. omega. Qed.
 
 Lemma not_lt: forall n m : nat, n <= m <-> ~ m < n.
-  intros. omega. Qed.
+Proof. intros. omega. Qed.
 
 Lemma not_le: forall n m : nat, ~ n <= m <-> m < n.
-  intros. omega. Qed.
+Proof. intros. omega. Qed.
 
 Lemma le_lt_S: forall n m : nat, S n <= m <-> n < m.
-  intros. omega. Qed.
+Proof. intros. omega. Qed.
 
 Lemma lt_S_le: forall n m : nat, n <= m <-> n < S m.
-  intros. omega. Qed.
+Proof. intros. omega. Qed.
 
 
-(* ****** REPEATED APPLICATION ************ *)
+(* ****** LEMMAS ABOUT REPEATED APPLICATION ****** *)
 
 Fixpoint repeat (f: nat -> nat) (rep n : nat) : nat :=
   match rep with
@@ -41,10 +43,14 @@ Fixpoint repeat (f: nat -> nat) (rep n : nat) : nat :=
 Theorem repeat_S_comm :
   forall f k n, repeat f (S k) n = repeat f k (f n).
 Proof.
-  induction k; trivial. 
+  induction k; [trivial|].
   intro. simpl in *. rewrite IHk. trivial.
 Qed.
 
 Theorem repeat_plus :
-  forall f k l n, repeat f (k + l) n = repeat f k (repeat f l n).
-Proof. induction k; trivial. simpl; intros; rewrite IHk; trivial. Qed.
+  forall f k l n, repeat f (k + l) n =
+                  repeat f k (repeat f l n).
+Proof.
+  induction k; [trivial|].
+  simpl; intros; rewrite IHk; trivial.
+Qed.
