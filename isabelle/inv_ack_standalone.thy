36694301872e41462fce256c252e7c9d051b2340
thbl.2012 <thbl.2012@gmail.com>
2019-07-29 11:37:20 +0800

    Added preliminary isabelle transaltion of standalone code for nat

---
+++ isabelle/inv_ack_standalone.thy
@@ -0,0 +1,30 @@
+theory inv_ack_standalone
+  imports Main
+begin
+
+(* Countdown worker function *)
+primrec cdn_wkr :: "(nat \<Rightarrow> nat) \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat" where
+"cdn_wkr f a n 0 = 0" |
+"cdn_wkr f a n (Suc k) = (if n \<le> a then 0 else Suc (cdn_wkr f a (f n) k))"
+
+(* Countdown *)
+fun countdown_to :: "(nat \<Rightarrow> nat) \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat" where
+"countdown_to f a n = cdn_wkr f a n n"
+
+(* Inverse Ackermann worker function *)
+primrec inv_ack_wkr :: "(nat \<Rightarrow> nat) \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat" where
+"inv_ack_wkr f n k 0 = k" |
+"inv_ack_wkr f n k (Suc b) = (if n \<le> k then k
+                              else let g = (countdown_to f 1) in
+                              inv_ack_wkr (g o f) (g n) (Suc k) b)"
+
+(* Inverse Ackermann function in linear time *)
+fun inv_ack_linear :: "nat \<Rightarrow> nat" where
+"inv_ack_linear 0 = 0" |
+"inv_ack_linear (Suc 0) = 0" |
+"inv_ack_linear (Suc (Suc n)) = inv_ack_wkr (\<lambda> x. (x - 2)) n 1 (Suc n)"
+
+value "inv_ack_linear 61"
+value "inv_ack_linear 62"
+
+end
