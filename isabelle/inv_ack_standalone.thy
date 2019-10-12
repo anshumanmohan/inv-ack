e4ce504cbc8bd3c0371fba99a7ad6ee1808d53db
Anshuman Mohan <anshumanmohan@live.com>
2019-10-12 10:21:46 +0800

    Isabelle attempt from long ago

--- isabelle/inv_ack_standalone.thy
+++ isabelle/inv_ack_standalone.thy
@@ -1,6 +1,43 @@
 theory inv_ack_standalone
-  imports Main
+  imports "HOL-Library.Log_Nat" HOL.Divides
 begin
+
+value "(4::nat) div (2::nat)"
+
+(* attempt at naive log *)
+(*
+primrec my_log :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+"my_log 0 b = 1" |
+"my_log a 0 = 0" |
+"my_log a (Suc 0) = 0" |
+"my_log a b = 1 + my_log a (b div a)"
+*)
+(* Fails because 
+Nonprimitive pattern in left-hand side at
+  my_log a (Suc 0) = 0
+*)
+
+lemma "1 + 1 = 2"
+  by auto
+
+function sum2 :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+"sum2 i N = (if i > N then 0 else i + sum2 (Suc i) N)"
+  by simp
+
+termination sum2
+  try
+
+function my_log :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+"my_log 0 0 = 1" |
+"my_log 0 b = 1" |
+"my_log a 0 = 0" |
+"my_log a (Suc 0) = 0" |
+"my_log a b = 1 + my_log a (b div a)"
+  by pat_completeness auto
+termination my_log
+  apply try
+ 
+value "floorlog (7::nat) 100"
 
 (* Countdown worker function *)
 primrec cdn_wkr :: "(nat \<Rightarrow> nat) \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat" where
@@ -25,6 +62,14 @@ fun inv_ack_linear :: "nat \<Rightarrow> nat" where
 "inv_ack_linear (Suc (Suc n)) = inv_ack_wkr (\<lambda> x. (x - 2)) n 1 (Suc n)"
 
 value "inv_ack_linear 61"
-value "inv_ack_linear 62"
+value "inv_ack_linear 100"
+value "inv_ack_linear 61"
+value "inv_ack_linear 1000"
+value "inv_ack_linear 61"
+value "inv_ack_linear 10000"
+value "inv_ack_linear 61"
+value "inv_ack_linear 100000"
+value "inv_ack_linear 61"
+value "inv_ack_linear 1000000"
 
 end
